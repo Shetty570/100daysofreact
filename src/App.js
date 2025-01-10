bb6a399139a3f5886b13ff7af54d673b9cd124a7
suhas shetty <suhasshetty570@gmail.com>
2025-01-10 18:40:21 -0500

    Day5 states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import TestComponent from "./components/TestComponent";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <TestComponent name="Suhas" age="25" />
-      <TestComponent name="Ranjani" age="25" />
+      <button onClick={() => setIsActive(!isActive)}>Click</button>
+      {isActive && <TestComponent name="Suhas" age="25" />}
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <TestComponent name="Ranjani" age={counter} />
       <TestComponent name="Sunil" age="25" />
     </div>
   );
