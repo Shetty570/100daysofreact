e59b9d8e0b40bb49bed06b2e94ba127da38653ee
suhas shetty <suhasshetty570@gmail.com>
2025-01-13 20:58:57 -0500

    Day6-7 handle events and consitional rendering

--- src/App.js
+++ src/App.js
@@ -7,16 +7,28 @@ function App() {
   const [isActive, setIsActive] = useState(false);
 
   const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState("");
 
   return (
     <div className="App">
-      <button onClick={() => setIsActive(!isActive)}>Click</button>
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? "hide" : "show"}
+      </button>
       {isActive && <TestComponent name="Suhas" age="25" />}
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
       <h1>{counter}</h1>
       <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
       <TestComponent name="Ranjani" age={counter} />
       <TestComponent name="Sunil" age="25" />
+      {isActive && (
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      )}
+      <p>{message}</p>
     </div>
   );
 }
